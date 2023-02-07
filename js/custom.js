0448c9b1e1358ed9db11546c19bf6f975ea20860
Rohit Tekchandani <tekchandanirohit859@gmail.com>
2023-02-07 15:29:23 +0530

    Worked on index.html and style.css

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,63 @@
-let menuIcon = document.querySelector("#menu-icon");
+$(document).ready(function() {
+	//Prevent Page Reload on all # links
+	$("body").on("click","a[href='#']", function(e){
+		e.preventDefault();
+	});
 
-let downArrow = document.querySelector("#down-arrow");
+	//placeholder
+	$("[placeholder]").each(function () {
+		$(this).attr("data-placeholder", this.placeholder);
+		$(this).bind("focus", function () {
+			this.placeholder = '';
+		});
+		$(this).bind("blur", function () {
+			this.placeholder = $(this).attr("data-placeholder");
+		});
+	});
+	
+	// On scroll Add Class
+	$(window).scroll(function(e) {
+		if($(window).scrollTop() > 200){
+			$(".wrapper").addClass('page-scrolled');
+		}
+		else{
+			$(".wrapper").removeClass('page-scrolled');
+		}
+	});	
 
-let leftSidebar = document.querySelector(".left--sidebar");
+	// Footer margin set for stick to bottom
+	function footerAdj(){
+		var footerH = $(".footer").innerHeight();
+		$(".footer").css({ "margin-top": -footerH});
+		$(".main-content").css({ "padding-bottom": footerH});
+	};
+	footerAdj();
+	$(window).resize(function() {
+		footerAdj();		
+	});
 
-let closeArrow = document.querySelector("#close-arrow");
+	// Add remove class when window resize finished
+	var $resizeTimer;
+	$(window).on("resize", function(e) {
+		if(!$("body").hasClass("window-resizing")){
+			$("body").addClass("window-resizing");
+		}
+		clearTimeout($resizeTimer);
+		$resizeTimer = setTimeout(function() {    
+			$("body").removeClass("window-resizing");
+		}, 250);
+	});	
 
+	// Add new js functions here -----------------------------------------------------------------
+	$(".toggle-sidebar,.overlay").click(function(){
+		$("body").toggleClass("sidebar-open")	
+	});
 
-let rightNavigation = document.querySelector(".right-navigation");
+	$(".btn-search,.btn-close-search").click(function () {
+		$("body").toggleClass("search-open");
+	});
 
-
-
-menuIcon.addEventListener("click", () => {
-    if(leftSidebar.classList.contains('open')){
-        leftSidebar.classList.remove('open');
-        leftSidebar.classList.add('close');
-    }
-
-
-    else if(leftSidebar.classList.contains('close')){
-        leftSidebar.classList.remove('close');
-        leftSidebar.classList.add('open');
-    }
-})
-
-closeArrow.addEventListener("click", () => {
-    rightNavigation.classList.toggle("display-none");
-    
-})
-
-function Setimage1(
-    document.getElementById('img1').src="./images/dashboard-default.svg";
-);
+	// Don't add anything below this --------------------------------------------------------------
+	// Add Class on Window Load
+	$("body").addClass("page-loaded");
+});
